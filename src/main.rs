9e809d8bad6aa6f9ef20fc09cc52c919e51a5fb4
RbertKo <myeongsku@gmail.com>
2021-05-15 23:27:42 +0900

    Create get_paths_from_args fn

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,17 @@ fn main() {
 
     println!("{:?}", test);
 }
+
+struct PathArgs {
+    from: String;
+    target: String;
+}
+
+fn get_paths_from_args(args: env::Args) -> PathArgs {
+    let _args = args.collect();
+
+    PathArgs {
+        from: _args[1].clone(),
+        target: _args[2].clone()
+    }
+}
