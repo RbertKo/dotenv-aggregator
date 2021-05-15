d2e71d41d3769ab4c12e990cbcd275fc80cb960d
RbertKo <myeongsku@gmail.com>
2021-05-16 02:23:52 +0900

    Fix some issue

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::env;
 
 fn main() {
-    let test = env::args();
+    let test = get_paths_from_args(env::args());
 
     println!("{:?}", test);
 }
 
+#[derive(Debug)]
 struct PathArgs {
-    from: String;
-    target: String;
+    from: String,
+    target: String,
 }
 
 fn get_paths_from_args(args: env::Args) -> PathArgs {
-    let _args = args.collect();
+    let _args: Vec<String> = args.collect();
 
     PathArgs {
         from: _args[1].clone(),
