66e346c7009069495ccfe7da2b60cdb7e3c8d2ca
RbertKo <myeongsku@gmail.com>
2021-06-01 23:44:13 +0900

    Eidt some

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,25 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 
 fn main() {
-    let test = PathArgs::new(env::args());
+    let test;
+
+    let _args: Vec<String> = env::args().collect();
+
+    if let (Some(_from), Some(_target)) = (_args.get(1), _args.get(2)) {
+        test = PathArgs::new(_from, _target);
+    } else {
+        panic!("You've to send arguments \"from\", \"target\"")
+    }
 
     let test = test.to_env_text();
 
     if let Ok(env) = test {
         let (mut test1, test2) = env;
 
-        test1.convert();
-
+        {
+            test1.convert();
+        }
+        
         println!("{:?}", test1.parsed_text);
 
         test1.update_text(test2.text);
@@ -25,15 +35,15 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct PathArgs {
-    from: String,
-    target: String,
+struct PathArgs<'a> {
+    from: &'a String,
+    target: &'a String,
 }
 
 #[derive(Debug)]
-struct EnvText {
-    text: String,
-    parsed_text: Option<HashMap<String, String>>,
+struct EnvText<'a> {
+    text: &'a String,
+    parsed_text: Option<HashMap<&'a str, String>>,
 }
 
 // #[derive(Debug)]
@@ -42,18 +52,14 @@ struct EnvText {
 //     Env(String, String),
 // }
 
-impl PathArgs {
-    pub fn new(args: env::Args) -> PathArgs {
-        let _args: Vec<String> = args.collect();
-        let from: String;
-        let target: String;
-    
-        if let (Some(_from), Some(_target)) = (_args.get(1), _args.get(2)) {
-            from = String::from(_from);
-            target = String::from(_target);
-        } else {
-            panic!("You've to send arguments \"from\", \"target\"")
-        }
+impl<'a> PathArgs<'a> {
+    pub fn new(from: &'a String, target: &'a String) -> PathArgs<'a> {
+        // if let (Some(_from), Some(_target)) = (_args.get(1), _args.get(2)) {
+        //     from = _from;
+        //     target = _target;
+        // } else {
+        //     panic!("You've to send arguments \"from\", \"target\"")
+        // }
     
         PathArgs {
             from,
@@ -75,21 +81,21 @@ impl PathArgs {
         let target_file = self.read_env_to_string(&self.target)?;
         
         Ok((
-            EnvText::new(from_file),
-            EnvText::new(target_file),
+            EnvText::new(&from_file),
+            EnvText::new(&target_file),
         ))
     }
 }
 
-impl EnvText {
-    pub fn new(text: String) -> EnvText {
+impl<'a> EnvText<'a> {
+    pub fn new(text: &'a String) -> EnvText<'a> {
         EnvText {
             text,
             parsed_text: None,
         }
     }
 
-    fn parse_line(&mut self, text_line: String) {
+    fn parse_line(&mut self, text_line: &'a str) {
         let index = text_line.find('=');
 
         if let None = self.parsed_text {
@@ -101,35 +107,34 @@ impl EnvText {
                 let key = &text_line[0 .. _index];
                 let value = &text_line[_index+1 .. text_line.len()];
 
-                _parsed_text.insert(String::from(key.trim()), String::from(value.trim()));
+                _parsed_text.insert(key.trim(), String::from(value.trim()));
             } else {
                 // _parsed_text.push(Item::Comment(text_line));
             }
         }
     }
 
-    pub fn update_text(&mut self, text: String) {
+    pub fn update_text(&mut self, text: &'a String) {
         self.text = text;
         self.parsed_text = None;
     }
 
-    pub fn convert(&mut self) {
-        let text_lines: Vec<String> = self.text.split('\n').map(|x| String::from(x)).collect();
-
-        for text in text_lines {
+    pub fn convert(&'a mut self) {
+        for text in self.text.split('\n') {
             self.parse_line(text);
         }
     }
 
-    pub fn migrate_from(&mut self, from: &EnvText) {
+    pub fn migrate_from(&mut self, from: &'a EnvText) {
         if self.parsed_text == None {
             // return Result::Err("This instance isn't converted yet.");
         }
 
         if let (Some(target_map), Some(from_map)) = (&mut self.parsed_text, &from.parsed_text) {
             for (key, value) in from_map {
-                target_map[key] = String::from(value);
-                
+                if let Some(target_value) = target_map.get_mut(key) {
+                    *target_value = value.to_string();
+                }
             }
         }
     }
