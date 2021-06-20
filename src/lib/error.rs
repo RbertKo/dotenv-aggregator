5f0b5cd1d48b1d2e02ae2844eacb8461a1f762ce
RbertKo <myeongsku@gmail.com>
2021-06-21 08:44:24 +0900

    Edit error.rs

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -4,3 +4,21 @@ use std::fmt;
 pub enum Error {
     
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match self {
+
+      }
+    }
+  }
+
+impl std::error:Error for Error {
+
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+
+    }
+}
