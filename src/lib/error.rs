cfbbc917d6eaadff12d2756406d7b4d25285b867
RbertKo <myeongsku@gmail.com>
2021-07-04 23:53:56 +0900

    Edit Error to detail

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -2,25 +2,27 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum Error {
-    IOError
+    IOError(std::io::Error)
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
       match self {
-        Self::IOError => write!(f, "Error occured because of IO"),
+        Self::IOError(_) => write!(f, "Error occured because of IO"),
       }
     }
   }
 
 impl std::error:Error for Error {
   fn source(&self) -> Option<&dyn Error + 'static> {
-
+    match self {
+      Self::IOError(ref error) => error
+    }
   }
 }
 
 impl From<std::io::Error> for Error {
-    fn from(_: std::io::Error) -> Self {
-      Self::IOError
+    fn from(error: std::io::Error) -> Self {
+      Self::IOError(error)
     }
 }
