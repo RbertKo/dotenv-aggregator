0d0e28e12bf19509cfcd3aa98985158e47f12f31
RbertKo <myeongsku@gmail.com>
2021-07-04 23:48:25 +0900

    Refactor error

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -2,23 +2,25 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum Error {
-    
+    IOError
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-      write!(f, "error!");
+      match self {
+        Self::IOError => write!(f, "Error occured because of IO"),
+      }
     }
   }
 
 impl std::error:Error for Error {
-  fn description() {
-    
+  fn source(&self) -> Option<&dyn Error + 'static> {
+
   }
 }
 
 impl From<std::io::Error> for Error {
     fn from(_: std::io::Error) -> Self {
-
+      Self::IOError
     }
 }
